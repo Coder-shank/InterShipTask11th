65b4983e28610042e5127dfe28221381c9244d85
Shashank Shekhar Pandey <91602965+Coder-shank@users.noreply.github.com>
2026-01-19 08:30:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(require("cors")());
+const cors = require("cors");
+app.use(cors({
+  origin: process.env.FRONTEND_URL, // set in .env for deploy
+  credentials: true,
+}));
+
 connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/blogs", require("./routes/blogRoutes"));
-app.listen(5000, () => console.log("Server running on 5000"));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
